40b4ead68e0cd31fc817e30f994f345f78695ca5
Onela25 <onelawellem0525@gmail.com>
2024-09-25 16:58:16 +0200

    Added a moment.js Timezone. and edited it.

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,20 @@
+
+//Los Ageles
 let losAngelesElement = document.querySelector("#Los-Angeles");
 let losAngelesDateElement = document.querySelector(".date");
 let losAngelesTimeElement = document.querySelector(".time");
-let losAngelesTime = moment();
+let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do yyyy") ;
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss[<small>]A[</small>]");;
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY") ;
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss[<small>]A[</small>]");
+
+
+
+//Cape Town
+let CapeTownElement = document.querySelector("#Cape-Town");
+let CapeTownDateElement = document.querySelector(".date");
+let CapeTownTimeElement = document.querySelector(".time");
+let CapeTownTime = moment().tz("South_Africa/Western_Cape");
+
+CapeTownDateElement.innerHTML = CapeTownTime.format("MMMM Do YYYY") ;
+CapeTownTimeElement.innerHTML = CapeTownTime.format("h:mm:ss[<small>]A[</small>]");
