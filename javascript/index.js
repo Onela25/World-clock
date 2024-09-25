fc36c9c4bf6504b1c05cc7f978d4bd249d629546
Onela25 <onelawellem0525@gmail.com>
2024-09-25 15:43:14 +0200

    Intergrated moments.js into the html. Added basic javascript.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,7 @@
-alert("hello!");
+let losAngelesElement = document.querySelector("#Los-Angeles");
+let losAngelesDateElement = document.querySelector(".date");
+let losAngelesTimeElement = document.querySelector(".time");
+
+losAngelesDateElement.innerHTML = "September 25th 2024";
+losAngelesTimeElement.innerHTML = "1:49:15 <small>AM</small>";
+
